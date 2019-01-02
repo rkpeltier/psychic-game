67713d7a2926bc506bccad1d00e4630f5313357b
rkpeltier <rkpeltier@gmail.com>
2019-01-02 14:25:09 -0800

    fixed js link to html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,31 @@
-var letters = ["a", "e", "i", "o", "u", "y"];
 
 var wins = 0;
 var losses = 0;
 
-//Change this variable to show correctly guessed letters document.write
-var guessCount = 0;
-
 var remainingGuesses = 0;
 
-//decrease remainingGuesses by 1 per key event until 0 remain, then game over
+//Change this variable to show correctly guessed letters document.write
 
-//add some function for letter guessed right, increase number of wins by one
+document.onkeyup = function(event) {
+    var letters = event.key;
 
-//for each letter guessed wrong, increase losses by one
+        if(letters === "a") {
+            console.log("a");
+        } else if(letters === "e") {
+            console.log("e");
+        } else if(letters === "i") {
+            console.log("i");
+        } else if(letters === "o") {
+            console.log("o");
+        } else if(letters === "u") {
+            console.log("u");
+        } else if(letters === "y") {
+            console.log("y");
+        }
+}
+
+
+//Call functions
 
 
 
