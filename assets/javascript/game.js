078194558a9c33d52e521254f20af79bd9c1299b
rkpeltier <rkpeltier@gmail.com>
2019-01-04 13:32:41 -0800

    updated js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,27 +6,22 @@ var remainingGuesses = 9;
 
 function countWins() {
     wins = wins++;
+    document.querySelector("#win").innerHTML = "Wins: " + wins;
 }
 
 function countLosses() {
     losses = losses++;
+    document.querySelector("#lose").innerHTML = "Losses: " + losses;
 }
 
 function decrementGuess() {
-    document.querySelector("#guessRemain").innerHTML = "Remaining Guesses " + remainingGuesses;
+    document.querySelector("#guessRemain").innerHTML = "Remaining Guesses: " + remainingGuesses;
 }
 
 document.onkeyup = function(event) {
-        if (gameLetters == gameLetters) {
-            countWins();
-        } else (gameLetters =/ gameLetters) {
-            countLosses();
-        }
-
-    
-    //If keys match gameLetters decrement guess & print correct answer #yourGuess
+    if (gameLetters === gameLetters) {
+        countWins();
+    } else countLosses();
 }
-
-
 
 
