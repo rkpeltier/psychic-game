b645d7507d6de6372964945397a2dd64b39839b4
rkpeltier <rkpeltier@gmail.com>
2019-01-04 13:42:02 -0800

    Mostly just adding notes in the comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,23 +4,32 @@ var wins = 0;
 var losses = 0;
 var remainingGuesses = 9;
 
+//Need to print text names to HTML always instead of just when user does something
+
+//Counting the wins
 function countWins() {
-    wins = wins++;
+    wins++;
     document.querySelector("#win").innerHTML = "Wins: " + wins;
 }
 
+//Counting losses
 function countLosses() {
-    losses = losses++;
+    losses++;
     document.querySelector("#lose").innerHTML = "Losses: " + losses;
 }
 
+//Guesses per game
 function decrementGuess() {
+    remainingGuesses --;
     document.querySelector("#guessRemain").innerHTML = "Remaining Guesses: " + remainingGuesses;
 }
 
+//Typing does things
 document.onkeyup = function(event) {
+    //Need a way to express if user types correct letters that it only then counts it as wins
     if (gameLetters === gameLetters) {
         countWins();
+        //while letting 
     } else countLosses();
 }
 
